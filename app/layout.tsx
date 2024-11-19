55d3704acfb4f74b668f3cf55a3a1de09c7b4aed
mhmd-aashik <aashikdevelop@gmail.com>
2024-11-19 19:58:31 +0530

    Assets and Metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,8 +17,12 @@ const spaceGrotesk = localFont({
 });
 
 export const metadata: Metadata = {
-  title: "DevFlow Next-15",
-  description: "A Next.js 15 a better way to build your app",
+  title: "Dev Overflow",
+  description:
+    "A community-driven platform for asking and answering programming questions. Get help, share knowledge, and collaborate with developers from around the world. Explore topics in web development, mobile app development, algorithms, data structures, and more.",
+  icons: {
+    icon: "/images/site-logo.svg",
+  },
 };
 
 export default function RootLayout({

dfbb883841082f6b1ae627f5aeca0381581b4eb1
mhmd-aashik <aashikdevelop@gmail.com>
2024-11-19 19:18:26 +0530

    Tailwind CSS Theming

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,7 @@
 import React from "react";
 
 const Home = () => {
-  return (
-    <h1 className="mt-1 bg-red-50 px-1 text-3xl font-bold text-purple-500">
-      Wel cm to the world of nextjs
-    </h1>
-  );
+  return <h1 className="h1-bold">Wel cm to the world of nextjs</h1>;
 };
 
 export default Home;

7aad169bcfce807f46ecc130a29f39ca6ab64a4d
mhmd-aashik <aashikdevelop@gmail.com>
2024-11-21 10:07:35 +0530

    Local Search Bar & query string npm & debounce

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,13 +1,23 @@
 import { auth, signOut } from "@/auth";
+import LocalSearch from "@/components/search/LocalSearch";
 import { Button } from "@/components/ui/button";
 import ROUTES from "@/constants/routes";
 import Link from "next/link";
 
-const Home = async () => {
+interface SearchParamsProps {
+  searchParams: Promise<{ [key: string]: string }>;
+}
+
+const Home = async ({ searchParams }: SearchParamsProps) => {
+  const { query = "" } = await searchParams;
+
+  const filteredQuestion = questions.filter((question) => {
+    return question.title.toLowerCase().includes(query?.toLowerCase());
+  });
   return (
     <>
       <section className="flex w-full flex-col-reverse sm:flex-row justify-between gap-4 sm:items-center">
-        <h1 className="text-bold text-dark100_light900">All Questions</h1>
+        <h1 className="h1-bold text-dark100_light900">All Questions</h1>
 
         <Button
           className="primary-gradient min-h-[46px] px-4 py-3 !text-light-900"
@@ -16,16 +26,53 @@ const Home = async () => {
           <Link href={ROUTES.ASK_QUESTION}>Ask a question</Link>
         </Button>
       </section>
-      <section className="mt-11">Local Search</section>
+      <section className="mt-11">
+        <LocalSearch
+          route="/"
+          imgSrc="/icons/search.svg"
+          placeholder="Search Questions..."
+          otherClasses="flex-1"
+        />
+      </section>
       Home Filters
       <div className="mt-10 flex w-full flex-col gap-6">
-        <p>Question card 1</p>
-        <p>Question card 2</p>
-        <p>Question card 3</p>
-        <p>Question card 4</p>
+        {filteredQuestion.map((question) => (
+          <h1 key={question._id}>{question.title}</h1>
+        ))}
       </div>
     </>
   );
 };
 
 export default Home;
+
+const questions = [
+  {
+    _id: "1",
+    title: "How to learn React?",
+    description: "I want to learn React, can anyone help me?",
+    tags: [
+      { _id: "1", name: "React" },
+      { _id: "2", name: "JavaScript" },
+    ],
+    author: { _id: "1", name: "John Doe" },
+    upvotes: 10,
+    answers: 5,
+    views: 100,
+    createdAt: new Date(),
+  },
+  {
+    _id: "2",
+    title: "How to learn JavaScript?",
+    description: "I want to learn JavaScript, can anyone help me?",
+    tags: [
+      { _id: "1", name: "React" },
+      { _id: "2", name: "JavaScript" },
+    ],
+    author: { _id: "1", name: "John Doe" },
+    upvotes: 10,
+    answers: 5,
+    views: 100,
+    createdAt: new Date(),
+  },
+];

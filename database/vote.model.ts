d29f13b78646aaac0aedd3b1861f87e33bb7bda3
mhmd-aashik <aashikdevelop@gmail.com>
2024-11-22 16:24:05 +0530

    Create Vote Model

---
+++ database/vote.model.ts
@@ -0,0 +1,39 @@
+import { model, models, Schema, Types } from "mongoose";
+
+export interface IVote {
+  author: Types.ObjectId;
+  id: Types.ObjectId;
+  type: "question" | "answer";
+  voteType: "upvote" | "downvote";
+}
+
+const VoteSchema = new Schema<IVote>(
+  {
+    author: {
+      type: Schema.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    id: {
+      type: Schema.ObjectId,
+      required: true,
+    },
+    type: {
+      type: String,
+      enum: ["question", "answer"],
+      required: true,
+    },
+    voteType: {
+      type: String,
+      enum: ["upvote", "downvote"],
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const Vote = models?.Vote || model<IVote>("Vote", VoteSchema);
+
+export default Vote;

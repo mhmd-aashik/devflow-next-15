e0d8ce4938f28bc31c0ca015f1060bb1bbc8f33e
mhmd-aashik <aashikdevelop@gmail.com>
2024-11-22 15:56:30 +0530

    Create Answer Tag

--- database/template.model.ts
+++ database/template.model.ts
@@ -2,7 +2,12 @@ import { model, models, Schema, Types } from "mongoose";
 
 export interface IModel {}
 
-const ModelSchema = new Schema<IModel>({});
+const ModelSchema = new Schema<IModel>(
+  {},
+  {
+    timestamps: true,
+  }
+);
 
 const Model = models?.Model || model<IModel>("Model", ModelSchema);
 
